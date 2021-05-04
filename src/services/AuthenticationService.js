b02d4d53ead0d55e836bd8d54e3c94f73336d437
Vladimir Maryanchuk <39910684+Hesowam@users.noreply.github.com>
2021-05-05 00:04:19 +0300

    feat: Added AuthenticationService.js

---
+++ src/services/AuthenticationService.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const server = require('../constants/server.js')
+
+class AuthenticationService {
+    login(username, password) {
+        console.log(server)
+        return axios( server.IP + server.PORT + server.DEFAULT_PATH + server.routes.authorization.login, {
+            method: 'POST',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+                withCredentials: true,
+            },
+            data: {
+                username: username,
+                password: password,
+            }
+        } ).then(res => {
+            if ( res.data.token ) {
+                localStorage.setItem( "user", JSON.stringify(res.data) );
+            }
+        }).catch(e => {console.log(e)});
+    }
+}
+
+export default AuthenticationService.prototype
