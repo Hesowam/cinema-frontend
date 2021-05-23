3722a44c5a74c3c42ddd012d78ae94eec9368cc8
Vladimir Maryanchuk <39910684+Hesowam@users.noreply.github.com>
2021-05-23 13:53:06 +0300

    fix

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,18 +1,4 @@
 module.exports = {
     IP: "http://localhost",
     PORT: ":8095",
-    DEFAULT_PATH: "/api/v1/",
-    routes: {
-        authorization: {
-            login: "auth/login",
-            registration: "auth/register"
-        },
-        open: {
-            path: "open/",
-            film: {
-                path: "film/",
-                all: "get",
-            }
-        }
-    }
 }
--- src/services/AuthenticationService.js
+++ src/services/AuthenticationService.js
@@ -5,7 +5,7 @@ const server = require('../constants/server.js')
 class AuthenticationService {
     login(username, password) {
         console.log(server)
-        return axios( server.IP + server.PORT + server.DEFAULT_PATH + server.routes.authorization.login, {
+        return axios( server.IP + server.PORT + `/api/v1/auth/login`, {
             method: 'POST',
             headers: {
                 'Access-Control-Allow-Origin': '*',
@@ -22,6 +22,22 @@ class AuthenticationService {
                 localStorage.setItem( "user", JSON.stringify(res.data) );
             }
         }).catch(e => {console.log(e)});
+    }
+
+    register(username, password, email) {
+        return axios( server.IP + server.PORT + `/api/v1/auth/register`, {
+            method: 'POST',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+                withCredentials: true,
+            },
+            data: {
+                username,
+                password,
+                email
+            }
+        } ).catch(e => {console.log(e); return "Registration error"});
     }
 }
 
